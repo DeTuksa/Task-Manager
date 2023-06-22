98b8dc94760131bd39f38522e6b90e6500c825f6
DeTuksa <emmydave414@gmail.com>
2023-06-22 19:44:13 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/asas/task_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.asas.task_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants/theme/tm_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TMTheme {
+
+  static ThemeData theme = ThemeData(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      background: Color(0xFFD8DEF3),
+      primary: Color(0xFF9C2CF3),
+      secondary: Color(0xFF3A48F8),
+      error: Colors.red,
+      onPrimary: Color(0xFF3A48F8),
+      onSecondary: Color(0xFF9C2CF3),
+      onError: Colors.white,
+      onBackground: Colors.black,
+      surface: Color(0xFFD8DEF3),
+      onSurface: Color(0xFF2E3A59)
+    )
+  );
+}
